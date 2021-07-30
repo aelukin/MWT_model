131867ffc80a05d3f8ba4660bd65744a603bd9bc
aelukin <86794870+aelukin@users.noreply.github.com>
2021-07-30 13:04:18 +0300

    Add files via upload

---
+++ MWT_model_input.m
@@ -0,0 +1,15 @@
+%this file contains input parameters for the model of the experimental wind
+%generator
+
+clear
+
+N=2; %number of cylinders
+R=0.6; %rotor radius, m
+r=0.065; %cylinder radius, m
+p=1.225; %air density, kg/m^3
+Vm=8; %wind speed, m/s
+wc=549; %rotational speed of the cylinder, rad/s
+Me=0.1; %mechanical load, Nm
+J=2; %rotational intertia of the rotor,kg*m^2
+b=1.4; %torsional damping, dimensionless. the presented system is overdamped
+
